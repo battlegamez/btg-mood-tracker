391bba25961bd82239978d723a223f358d7986c3
battlegamez <ultralanpasalan0@gmail.com>
2022-01-23 03:08:07 +0000

    eee

--- svelte.config.js
+++ svelte.config.js
@@ -11,3 +11,14 @@ const config = {
 };
 
 export default config;
+import vercel from '@sveltejs/adapter-vercel';
+
+const config = {
+    kit: {
+   	 // hydrate the <div id="svelte"> element in src/app.html
+   	 target: '#svelte',
+   	 adapter: vercel()
+    }
+};
+
+export default config;
